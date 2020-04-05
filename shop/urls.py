20b8749e00f6aa55161c31339ae82bfe04c4376f
Yash Marmat <yashmarmat08@gmail.com>
2020-04-05 18:23:47 +0530

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import userdisplay, homepage
 
 urlpatterns = [
-    path('', homepage),
-    path('products/', userdisplay),
+    path('', homepage, name = 'home'),
+    path('products/', userdisplay, name = 'index'),
 ]
